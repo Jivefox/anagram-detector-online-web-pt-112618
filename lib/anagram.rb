8ff153a79c7abbb38ce84f982dca5419ba0e9456
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-06 23:27:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,16 +9,3 @@ class Anagram
     words.select {|word| word.split("").sort == @word.split("").sort}
   end
 end
-  
-  
-# class Anagram
-#   attr_accessor :word
-
-#   def initialize(word)
-#     @word = word
-#   end
-
-#   def match(array)
-#     array.select {|x| x.split(//).sort == @word.split(//).sort}
-#   end
-# end
